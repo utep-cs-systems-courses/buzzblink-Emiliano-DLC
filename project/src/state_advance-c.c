f854019f466a13fa7f335c72334d5177dccb89e8
Emiliano-DLC <60456070+Emiliano-DLC@users.noreply.github.com>
2022-07-28 15:28:21 -0600

    Create state_advance-c.c

---
+++ project/src/state_advance-c.c
@@ -0,0 +1,28 @@
+#include <msp430.h>
+#include <time.h>
+#include "led.h"
+#include "buzzer.h"
+#include "switches.h"
+#include "stateMachines.h"
+#include "state_advance.h"
+
+void state_advance(c)		/* alternate between toggling red & green */
+{
+  switch (c){
+    case 1:
+    SWone();
+    break;
+
+    case 2:
+    SWtwo();
+    break;
+
+    case 3:
+    SWtree();
+    break;
+
+    case 4:
+    SWfour();
+    break;
+  }
+}
