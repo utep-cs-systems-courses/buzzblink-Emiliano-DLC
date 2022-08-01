6c4a14408d7c96fa0d4446a3c7d8fc6abc41fb7c
Emiliano-DLC <60456070+Emiliano-DLC@users.noreply.github.com>
2022-08-01 16:13:09 -0600

    Update switches.c

--- project/src/switches.c
+++ project/src/switches.c
@@ -4,8 +4,6 @@
 #include "buzzer.h"
 #include "stateMachines.h"
 
-short switchSM;
-
 //TODO: Change P1 and p1 to P2 and p2 respectively
 
 //Status for each button
@@ -52,10 +50,10 @@ switch_interrupt_handler()
   char sw4_curr_stat = (p2val & SW4) ? 0 : 1; /* 0 when SW4 is up */
  
 
-  if (sw1_curr_stat == 1){switchSM = 1;}
-  else if(sw2_curr_stat == 1){switchSM = 2;}
-  else if(sw3_curr_stat == 1){switchSM = 3;}
-  else if(sw4_curr_stat == 1){switchSM = 4;} 
+  if (sw1_curr_stat == 1){switchSM = 0;}
+  else if(sw2_curr_stat == 1){switchSM = 1;}
+  else if(sw3_curr_stat == 1){switchSM = 2;}
+  else if(sw4_curr_stat == 1){switchSM = 3;} 
   state_advance(switchSM);
   
 }
