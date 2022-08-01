6dad140629ad192aa04f734ba309a495d2e2681c
Emiliano-DLC <60456070+Emiliano-DLC@users.noreply.github.com>
2022-08-01 16:24:10 -0600

    Update stateMachines.c

--- project/src/stateMachines.c
+++ project/src/stateMachines.c
@@ -5,46 +5,12 @@
 #include "buzzer.h"
 #include "switches.h"
 
-void toggle_red()		/* always toggle! */
-{
-  static char state = 0;
-  red_on = 1;
-  green_on = 0;
-  led_changed = 1;
-  led_update();
-}
-
-void toggle_green()	/* only toggle green if red is on!  */
-{
-  red_on = 0;
-  green_on = 1;
-  led_changed = 1;
-  led_update();
-}
-
-void turnOff(){
-  red_on = 0;
-  green_on = 0;
-  led_changed = 1;
-  led_update();
-}
-
-void turnOn(){
-  red_on = 0;
-  green_on = 0;
-  led_changed = 1;
-  led_update();
-  buzzer_set_period(0);
-}
-
-
 void SWone(){
   red_on = 1;
   green_on = 1;
   led_changed = 1;
   led_update();
   buzzer_set_period(5000);
-
 }
 
 void SWtwo(){
@@ -55,11 +21,11 @@ void SWtwo(){
 }
 
 void SWtree(){
-  red_on ^= 1;
-  green_on = 1;
+  red_on = 1;
+  green_on = 0;
   led_changed = 1;
   led_update();
-  buzzer_set_period(1000);
+  buzzer_set_period(2500);
 }
 
 void SWfour(){
