1e47b4f28488e07ff2361a110dc2e03130555641
Emiliano-DLC <60456070+Emiliano-DLC@users.noreply.github.com>
2022-08-02 09:36:52 -0600

    Update stateMachines.h

--- project/src/stateMachines.h
+++ project/src/stateMachines.h
@@ -1,15 +1,6 @@
 #ifndef stateMachine_included
 #define stateMachine_included
 
-void toggle_red();
-void toggle_green();
-void turnOff();
-void turnOn();
-void SWone();
-void SWtwo();
-void SWtree();
-void SWfour();
-
 void state_advance();
 
 #endif // included
