bb17f78620adca1762ff139c5526992ea0362480
Emiliano-DLC <60456070+Emiliano-DLC@users.noreply.github.com>
2022-08-03 10:27:50 -0600

    Update wdInterruptHandler.c

--- project/src/wdInterruptHandler.c
+++ project/src/wdInterruptHandler.c
@@ -9,7 +9,9 @@ __interrupt_vec(WDT_VECTOR) WDT(){	/* 250 interrupts/sec */
   static char blink_count = 0;
   if (++blink_count == 150) {
     SWone();
-
-    blink_count = 0;
+    }
+    else{
+      blink_count = 0;
+      }
   }//
 }
