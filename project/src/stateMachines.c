5a90a4d6c74c366a8fd50786566965c2b2786f7c
Emiliano-DLC <60456070+Emiliano-DLC@users.noreply.github.com>
2022-08-03 10:13:38 -0600

    Update stateMachines.c

--- project/src/stateMachines.c
+++ project/src/stateMachines.c
@@ -7,41 +7,41 @@
 static char state = 0;
 
 void SWone(){
-  //static char state = 0;
   switch(state){
   case 0:
     red_on = 1;
     green_on = 1;
-    //led_changed = 1;
-    //led_update();
+    led_changed = 1;
+    led_update();
     buzzer_set_period(5000);
     state = 1;
     break;
   case 1:
     red_on = 1;
     green_on = 1;
-    //led_changed = 1;
-    //led_update();
+    led_changed = 1;
+    led_update();
       buzzer_set_period(10000);
       state = 2;
     break;
   case 2:
   red_on ^= 1;
   green_on = 1;
-  //led_changed = 1;
-  //led_update();
+  led_changed = 1;
+  led_update();
       buzzer_set_period(7500);
       state = 3;
     break;
   case 3:
   red_on = 1;
   green_on ^= 1;
-  //led_changed = 1;
-  //led_update();
+  led_changed = 1;
+  led_update();
       buzzer_set_period(2500);
       state = 0;
     break;
     }
+    SWone();
 }
 
 void SWtwo(){
