b55b08b4216713eb9c01c42f4f74c5a57c574d33
Emiliano-DLC <60456070+Emiliano-DLC@users.noreply.github.com>
2022-08-03 10:27:17 -0600

    Update wdInterruptHandler.c

--- project/src/wdInterruptHandler.c
+++ project/src/wdInterruptHandler.c
@@ -1,10 +1,14 @@
 #include <msp430.h>
 #include "stateMachines.h"
+#include "buzzer.h"
+#include "led.h"
+#include "switches.h"
 
 void
 __interrupt_vec(WDT_VECTOR) WDT(){	/* 250 interrupts/sec */
   static char blink_count = 0;
-  if (++blink_count == 125) {
+  if (++blink_count == 150) {
+    SWone();
 
     blink_count = 0;
   }//
